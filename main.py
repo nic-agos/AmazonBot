75fdc4d1e81769e86a5e6c8bd4ec954419b6ffc6
Niccolò Agostinelli <nic-agos@users.noreply.github.com>
2021-12-14 22:03:57 +0100

    Added file to repo

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""app.py: Primitive webscraping script"""
+
+#from configFile import * #REVIEW
+import bot
+import parseConfig
+
+if __name__ == '__main__':
+    getToken = parseConfig.ParseJson()
+    botToken = getToken.getConfig()
+    theBot = bot.RespondToCommands(botToken)
+    theBot.initializeBot()
+
